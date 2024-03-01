edb14461d82e6b164e7e638d0aabf6a388a4bfe1
rekhaaryal <aryalr458@gmail.com>
2024-03-01 07:45:17 +0545

    variable

---
+++ variable.js
@@ -0,0 +1,14 @@
+//variable is  the container(box) that holds the values.
+//they must start with a letter , an underscore or dollar sign.
+//case sensative , written in camelCase , cannot contain space.
+
+var myAge = 23 ;
+console.log(myAge);
+
+//Questions 
+// var my_firstName = "Rekha"; =>valid
+// var _myLastName$ = "Rama"; =>valid
+// var 123myAge = 25;=>invalid
+// var $cityName = "Kathmandu";=> valid
+// var my@Email = "aryalr458@gmail.com"; =>invalid
+

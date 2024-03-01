c0c9cdcb7483cfa31c617f5032143ea12345c2a2
rekhaaryal <aryalr458@gmail.com>
2024-03-01 18:11:41 +0545

    more in dataType (learn about parseInt , parseFloat , truthly and falsly value)

--- 1_dataType.js
+++ 1_dataType.js
@@ -42,3 +42,39 @@ console.log(typeof Number(myFavNum2));
 var str1 = 5;
 console.log(typeof String(str1));
 console.log(typeof( str1 + ""));
+//?7 Explain the concept of truthly and falsy values in javascript . Provide example
+// Truthly value (true , non empty string "hello", non zero number"34" , array and object)
+// falsely value(false , o , "", null, undefined , NAN)
+//? To check if a non-empty string is truthly or falsly in javascript , we can directly use if statement.
+
+var myName = '';
+if(myName){
+    console.log('this is truthly value');
+}
+else{
+    console.log('this is falsely value ');
+}
+var myName = 'Rekha Aryal'
+if(myName){
+    console.log('this is truthly value');
+}
+else{
+    console.log('this is falsely value ');
+}
+// parseInt & parseFloat are both functions in javascript used for converting string to number,but they have different use cases. 
+// parseInt is used to parse a string and convert it to an integer(whole number).
+var myName = "23";
+var myNumber = parseInt(myName);
+console.log(myNumber);
+//parseFloat is used to parse a string and convert it to a floating point number(decimal number).
+var myName = "23.2222";
+var myNumber = parseFloat(myName);
+console.log(myNumber);
+console.log(parseInt("123"));//123
+console.log(parseInt("123",10));//123
+console.log(parseInt("  123  "));//123 (Whitespace is ignore)
+console.log(parseInt("023"));//23 (0 is ignore)
+console.log(parseInt("3.22"));//3(decimal part is truncated)
+console.log(parseInt("$22"));
+console.log(parseInt("-322"));
+console.log(parseInt("xyz"));

b8a565bf0703095d1535e6696922cdb5d4396dc7
rekhaaryal <aryalr458@gmail.com>
2024-03-02 09:44:26 +0545

    learn about arithmetic operator

---
+++ 3_Expression&Operators.js
@@ -0,0 +1,31 @@
+// Types of operators (Arithmetic , comparision , logical, string, unary, ternary, type, assignment)
+//1. Assignment operator are symbols used to assign values to variables. they takes the value on the right side of the operators and assign it to the variable on the left side.
+var myfavNum = 15;//= is assignment operator 
+var myName ="Rekha";
+// 2. arithmetic operator
+var x = 5;
+var y = 6;
+var sum = x + y; 
+console.log(sum);
+var x = 5;
+var y = 6;
+var D = x - y; 
+console.log(D);
+var x = 5;
+var y = 6;
+var M = x * y; 
+console.log(M);
+var x = 5;
+var y = 6;
+var quotient = x / y; 
+console.log(quotient);
+// Modulus (%): Return the remainder when the left operand is divided by the right operand.
+var c = 17;
+var d = 5;
+var remainder = c % d;
+console.log(remainder);
+//*    interview Question
+//? What will be the output
+var result = "rekha" / 2;
+console.log(result);
+
